f5e733b3f2755a468586ac45f39a02caba236040
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2024-04-20 12:43:07 +0200

    Correct the font import and made the acme logo work again.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter, Lusitana } from 'next/font/google';
 
 
 export const inter = Inter({ subsets:['latin'] });
-export const lust = Inter({ subsets: ['latin'], weight: '800' });
+export const lusitana  = Lusitana({ subsets: ['latin'], weight: '700' });
